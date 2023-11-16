a489cbfab463bc846b26ee285866ad54bd788a37
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-11-16 22:44:32 +0200

    Update readFile.c

--- readFile.c
+++ readFile.c
@@ -8,15 +8,14 @@
 
 void readFile(FILE *fd)
 {
-	int linNum = 1, f = 0;
-	char *buff = NULL;
-	size_t length = 0;
+int linNum = 1, f = 0;
+char *buff = NULL;
+size_t length = 0;
 
-	while (getline(&buff, &length, fd) != -1)
-	{
-		f = parseLine(buff, linNum, f);
-		linNum++;
-	}
-	
-	free(buff);
+while (getline(&buff, &length, fd) != -1)
+{
+f = parseLine(buff, linNum, f);
+linNum++;
+}
+free(buff);
 }
